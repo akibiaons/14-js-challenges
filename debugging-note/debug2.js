58d849180f6e68300a42f5fde2aa48299838765b
Arthur Fernandez <arthur@snooget.com>
2023-05-03 17:37:24 -0700

    used console.log() to debug

--- debugging-note/debug2.js
+++ debugging-note/debug2.js
@@ -1,5 +1,7 @@
 const button = document.getElementById('button');
-const input = document.getElementById('textElem');
+const input = document.getElementById('textElement'); // was textElem
+
+console.log(input);
 
 button.addEventListener('click', () => {
     console.log(input.value);
